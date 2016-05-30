c87cf90b2502fb856cc6d827aae04e430dbe0ed9
Tero Lahtinen <tero.lahtinen@gelo.fi>
2016-05-30 09:15:20 +0300

    Reset shipping cost after order

--- app/code/community/Codaone/Logitrail/Model/Observer.php
+++ app/code/community/Codaone/Logitrail/Model/Observer.php
@@ -15,6 +15,7 @@ class Codaone_Logitrail_Model_Observer  extends Varien_Event_Observer {
         $logitrailId = $order->getLogitrailOrderId();
         $api->confirmOrder($logitrailId);  
         $order->addStatusHistoryComment(Mage::helper('logitrail')->__('Logitrail Order Id: ' . $logitrailId));
+        Mage::getSingleton('core/session')->setLogitrailShippingCost(0)
         if(Mage::getModel('logitrail/carrier_logitrail')->isTestMode()) {
             Mage::log("Confirmed order $logitrailId", null, 'logitrail.log');
         }
